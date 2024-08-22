7ec642c48a0b7846cd0f02866e5c588972f0ca88
Akio Kanashiki <akio@mail.com>
2024-08-22 22:48:25 +0900

    clear

--- points-webapi/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO account ( branch_code, account_number) VALUES ('001', '0000000');
-INSERT INTO account ( branch_code, account_number) VALUES ('101', '0000000');
-INSERT INTO account ( branch_code, account_number) VALUES ('201', '0000000');
