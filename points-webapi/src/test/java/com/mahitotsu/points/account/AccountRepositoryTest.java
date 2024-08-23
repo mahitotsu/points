aceab376132271410638572fdd8efba437163a0d
akring <akio@mailcom>
2024-08-23 17:54:51 +0900

    modified

--- points-webapi/src/test/java/com/mahitotsu/points/account/AccountRepositoryTest.java
+++ points-webapi/src/test/java/com/mahitotsu/points/account/AccountRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.mahitotsu.points.account;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.mahitotsu.points.TestBase;
 
 public class AccountRepositoryTest extends TestBase {
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Test
     public void testOpenAndCloseAccount() throws NullPointerException {
